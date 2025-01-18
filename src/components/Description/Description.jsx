c39a2aa31f6e9fffa6dc27d5de899dbd671d84fa
Svitlana Markevych <svetlana2507t@gmail.com>
2025-01-18 19:48:30 +0100

    styles update

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,7 +3,9 @@ import css from './Description.module.css';
 function Description() {
   return (
     <>
-      <h1 className={css.title}>Sip Happens Café</h1>
+      <h1 className={css.title}>
+        Sip Happens <span className={css.span}>Café</span>
+      </h1>
       <p className={css.text}>
         Please leave your feedback about our service by selecting one of the options below.
       </p>
