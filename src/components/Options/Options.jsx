4b9dae381ec269b5188dae600b32f308d406029a
Svitlana Markevych <svetlana2507t@gmail.com>
2025-01-18 07:32:52 +0100

    3 useStates + reset function

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,25 +1,41 @@
 import { useState } from 'react';
 import css from './Options.module.css';
+
 const Options = () => {
-  const [clicks, setClicks] = useState(0);
-  const handleClick = () => {
-    console.log('+1 click');
-    setClicks(clicks + 1);
-    console.log('clicks:', clicks);
+  const [rateGood, setRateGood] = useState(0);
+  const [rateNeutral, setRateNeutral] = useState(0);
+  const [rateBad, setRateBad] = useState(0);
+
+  const handleClickGood = () => {
+    console.log(rateGood);
+    setRateGood(rateGood + 1);
   };
-  const handleResetClick = () => {};
+  const handleClickNeutral = () => {
+    setRateNeutral(rateNeutral + 1);
+    console.log(rateNeutral);
+  };
+  const handleClickBad = () => {
+    console.log(rateBad);
+    setRateBad(rateBad + 1);
+  };
+  const handleClickReset = () => {
+    setRateGood(0);
+    setRateNeutral(0);
+    setRateBad(0);
+  };
+
   return (
     <div className={css.btn_wrapper}>
-      <button className={css.btn} onClick={() => handleClick}>
+      <button className={css.btn} onClick={handleClickGood}>
         Good
       </button>
-      <button className={css.btn} onClick={() => handleClick}>
+      <button className={css.btn} onClick={handleClickNeutral}>
         Neutral
       </button>
-      <button className={css.btn} onClick={() => handleClick}>
+      <button className={css.btn} onClick={handleClickBad}>
         Bad
       </button>
-      <button className={css.btn} onClick={() => handleResetClick}>
+      <button className={css.btn} onClick={handleClickReset}>
         Reset
       </button>
     </div>
